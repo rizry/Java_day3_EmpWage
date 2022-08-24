9a117fb33c2d348d2b2aa51dd97d719c2705d4dd
rizry <rizzwan.ansarii@gmail.com>
2022-08-24 18:31:20 +0530

    added part time work functionality

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -16,21 +16,24 @@ public class EmpWageComputation{
 
   private static String checkAttendance() {
     Random random = new Random();
-    boolean randomBool = random.nextBoolean();
+    int randomNum = random.nextInt(0, 4);
 
-    if (randomBool) return "Employee is present.";
-    else return "Employee is absent.";
+    if (randomNum == 1) return "employee is present full time.";
+    else if (randomNum == 2) return "employee is present part time.";
+    else return "employee is absent.";
 
   }
 
   private static int getDailyWage(String str) {
 
-   boolean isPresent = str.contains("present") || str.contains("Present");
+   Byte empRatePerHr = 20;
+   Byte hrsWorked = 0;
+   Byte fullTimeHrs = 8;   //
+   Byte partTimeHrs = 4;   //assumed
 
-   int empRatePerHr = 20;
-   int hrsWorked = 0;
+   if (str.contains("full time")) hrsWorked = fullTimeHrs;
+   else if (str.contains("part time")) hrsWorked = partTimeHrs;
 
-   if (isPresent) hrsWorked = 8;
    int dailyWage = empRatePerHr * hrsWorked;
    return dailyWage;
  }
