1c6f042acb36b6b44330ff8d68e227cdf6d1e2d9
rizry <rizzwan.ansarii@gmail.com>
2022-08-24 17:12:56 +0530

    added attendance check functionality

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -6,6 +6,18 @@ public class EmpWageComputation{
 
   public static void main(String[] args) {
     System.out.println("--Welcome to Employee Wage Computation Program--\n");
-  
+
+    String attendanceString = EmpWageComputation.checkAttendance();
+    System.out.println(attendanceString);
+
+  }
+
+  private static String checkAttendance() {
+    Random random = new Random();
+    boolean randomBool = random.nextBoolean();
+
+    if (randomBool) return "Employee is present.";
+    else return "Employee is absent.";
+
   }
 }
