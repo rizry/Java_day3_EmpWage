18134b88b06f58ce133791c6db3700d8e021870a
rizry <rizzwan.ansarii@gmail.com>
2022-08-24 17:05:14 +0530

    starting with the welcome message

---
+++ EmpWageComputation.java
@@ -0,0 +1,11 @@
+package com.java.day3;
+
+import java.util.Random;
+
+public class EmpWageComputation{
+
+  public static void main(String[] args) {
+    System.out.println("--Welcome to Employee Wage Computation Program--\n");
+  
+  }
+}
