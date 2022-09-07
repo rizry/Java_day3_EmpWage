622eb4df372cc5763db85f78a45d11b1f3c8a654
rizry <rizzwan.ansarii@gmail.com>
2022-09-07 20:17:16 +0530

    refactored code with class method and added minor changes

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,20 +4,20 @@ import java.util.Random;
 
 public class EmpWageComputation{
 
-  private static Byte empRatePerHr = 20;    //declaring the constants
-  private static Byte fullTimeHrs = 8;
-  private static Byte partTimeHrs = 4;
+   static Byte empRatePerHr = 20;    //declaring the constants
+   static Byte fullTimeHrs = 8;
+   static Byte partTimeHrs = 4;
 
   public static void main(String[] args) {
     System.out.println("--Welcome to Employee Wage Computation Program--\n");
 
-    System.out.println("Employee's wage for this month: " + getMonthlyWage() + "$");
+    System.out.println("Employee's wage for this month: " + new EmpWageComputation().getMonthlyWage() + "$");
 
   }
 
-  private static String checkAttendance() {
+   String checkAttendance() {
     Random random = new Random();
-    int randomNum = random.nextInt(0, 4);
+    int randomNum = random.nextInt(0, 3);
 
     switch (randomNum) {
       case 1:
@@ -32,7 +32,7 @@ public class EmpWageComputation{
 
   }
 
-  private static int getDailyWage() {
+   int getDailyWage() {
 
    Byte hrsWorked = 0;
    String str = checkAttendance();
@@ -45,13 +45,16 @@ public class EmpWageComputation{
  }
 
 
-  private static int getMonthlyWage() {
+  int getMonthlyWage() {
     int monthlyWage = 0;
     short daysWorked = 0;
     short hoursWorked = 0;
 
+    System.out.print("Employee wage each day");
+
     while (daysWorked < 20 && hoursWorked < 100) {
       int dailyWage = getDailyWage(); //getting employee's daily wage for each day
+      System.out.print(" " + dailyWage);
 
       String str = (dailyWage == 0) ? "absent" : dailyWage / EmpWageComputation.empRatePerHr == EmpWageComputation.fullTimeHrs ? "full time" : "part time";
 
@@ -67,6 +70,7 @@ public class EmpWageComputation{
       }
     }
 
+    System.out.println("\n");
     System.out.println("Employee has worked " + hoursWorked + " hours in "+daysWorked +" days this month.\n");
 
     monthlyWage= hoursWorked * EmpWageComputation.empRatePerHr;
@@ -74,4 +78,4 @@ public class EmpWageComputation{
     return monthlyWage;
   }
 
-}v
+}
